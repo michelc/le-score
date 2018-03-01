fbf3c1c92563a3324bf5c05d7788ec2b8bb35e36
Michel <michelc@gmail.com>
2018-03-01 16:17:21 +0100

    Correction path du scope.
    Avant : The path of the provided scope ('/') is not under the max scope
    allowed ('/le-score/').

--- rk.js
+++ rk.js
@@ -426,7 +426,7 @@ function beep(message) {
 function registerServiceWorker() {
   // enregistre le script sw avec les navigateurs qui le gèrent
   if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js", { scope: "/" }).then(() => {
+    navigator.serviceWorker.register("sw.js", { scope: "/le-score/" }).then(() => {
       console.log("Service Worker enregistré correctement.");
     }).catch(error => {
       console.log("Erreur lors de l'enregistrement du Service Worker : ", error);
