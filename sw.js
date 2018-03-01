79d0e5f995c0f304eac3e0979d6da3915cfdc9b1
Michel <michelc@gmail.com>
2018-03-01 16:20:45 +0100

    Correction urls car sous-domaine.

--- sw.js
+++ sw.js
@@ -6,19 +6,18 @@ self.addEventListener('install', e => {
     caches.open('ls-cache-pwa').then(cache => {
       // Ajoute toutes les URLs des éléments à mettre en cache
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/normalize.css',
-        '/le-score.css',
-        '/favicon.ico',
-        '/icons/apple-touch-icon.png',
-        '/icons/favicon-32x32.png',
-        '/icons/favicon-16x16.png',
-        '/icons/safari-pinned-tab.svg',
-        '/icons/browserconfig.xml',
-        '/icons/android-chrome-192x192.png',
-        '/icons/android-chrome-512x512.png',
-        '/rk.js',
+        'index.html',
+        'normalize.css',
+        'le-score.css',
+        'favicon.ico',
+        'icons/apple-touch-icon.png',
+        'icons/favicon-32x32.png',
+        'icons/favicon-16x16.png',
+        'icons/safari-pinned-tab.svg',
+        'icons/browserconfig.xml',
+        'icons/android-chrome-192x192.png',
+        'icons/android-chrome-512x512.png',
+        'rk.js',
       ]);
     })
   );
